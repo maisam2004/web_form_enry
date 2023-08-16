d1e6159a3a47db6c2da72a62fb96f8dc76c5c5d9
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-08-16 15:39:04 +0100

    Add extracted method to create lable and input

--- webform.py
+++ webform.py
@@ -8,8 +8,14 @@ class FormApp:
         self.root.geometry("450x450")
         self.create_form()
     def create_form(self):
+        self._extracted_from_create_form("Name:")
+        self._extracted_from_create_form("Family name:")
+        self._extracted_from_create_form("Mobiel Number:")
+        self._extracted_from_create_form("Post Code:")
+    # TODO Rename this here and in `create_form`
+    def _extracted_from_create_form(self, text):
         #lables
-        tk.Label(self.root,text="Name:",font=("Arial",16)).pack()
+        tk.Label(self.root, text=text, font=("Arial",16)).pack(padx=8,pady=8)
         self.name_entry = tk.Entry(self.root,width=40)
         self.name_entry.pack(padx=1,pady=1)
 
