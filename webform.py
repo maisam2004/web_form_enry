c6083a7f09bd1470df5c335c4a9aaef75f9854eb
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-08-16 15:11:42 +0100

    Create class formapp

--- webform.py
+++ webform.py
@@ -1,2 +1,7 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox 
+class FormApp:
+    def __init__(self,root):
+        self.root = root
+        self.root.title("User Info Form")
+        
