132e9a96d0569f688d2834380515aabeb866fb7d
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-08-16 15:57:59 +0100

    Modify padding of lables and submit

--- webform.py
+++ webform.py
@@ -9,27 +9,27 @@ class FormApp:
         self.create_form()
     def create_form(self):
         #lables
-        tk.Label(self.root,text="Name:",font=("Arial",16)).pack()
+        tk.Label(self.root,text="Name:",font=("Arial",16)).pack(padx=10,pady=10)
         self.name_entry = tk.Entry(self.root,width=40)
         
         self.name_entry.pack(padx=1,pady=1)
 
 
-        tk.Label(self.root,text="Family name:",font=("Arial",16)).pack()
+        tk.Label(self.root,text="Family name:",font=("Arial",16)).pack(padx=4,pady=4)
         self.name_entry = tk.Entry(self.root,width=40)
         self.name_entry.pack(padx=1,pady=1)
 
-        tk.Label(self.root,text="Mobile:",font=("Arial",16)).pack()
+        tk.Label(self.root,text="Mobile:",font=("Arial",16)).pack(padx=4,pady=4)
         self.name_entry = tk.Entry(self.root,width=40)
         self.name_entry.pack(padx=1,pady=1)
 
-        tk.Label(self.root, text="Post Code:",font=("Arial",16)).pack()
+        tk.Label(self.root, text="Post Code:",font=("Arial",16)).pack(padx=4,pady=4)
         self.postcode_entry = tk.Entry(self.root,width=40)
         self.postcode_entry.pack(padx=1,pady=1)
 
         # Submit Button
         self.submit_button = tk.Button(self.root, text="Submit",font=("Arial",14))
-        self.submit_button.pack(padx=2,pady=2)
+        self.submit_button.pack(padx=10,pady=30)
 
 if __name__ == "__main__":
     root = tk.Tk()
