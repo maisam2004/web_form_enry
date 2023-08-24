630d36a3bc9d6ff5cfa5b9bb2ad997bf150a9f3c
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-08-24 11:50:38 +0100

    Create lable inside user info

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ window = tkinter.Tk() #name root window
 window.title("Data Entery form") #add title to window
 
 frame = tkinter.Frame(window) #create frame inside window
+frame.pack()
+#create three lable frame in frame ,width and height ,3 rows and one column
+
+user_info_frame = tkinter.LabelFrame(frame,text="User Information")
+user_info_frame.grid(row=0,column=0)
+
+
+
 
 window.mainloop() #come up with it
