e2f8a8ff743ba7267064bc7b3d9350b3cde90446
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-08-24 12:04:34 +0100

    create lables,entris for first last names

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ frame.pack()
 user_info_frame = tkinter.LabelFrame(frame,text="User Information")
 user_info_frame.grid(row=0,column=0)
 
+#inside user info frame
+
+first_name_lable = tkinter.Label(user_info_frame,text="First Name")
+first_name_lable.grid(row=0 ,column=0)
+
+last_name_lable = tkinter.Label(user_info_frame,text="Last Name")
+last_name_lable.grid(row=0,column=1)
+
+first_name_entry  = tkinter.Entry(user_info_frame)
+first_name_entry.grid(row=1,column=0)
+
+last_name_entry = tkinter.Entry(user_info_frame)
+last_name_entry.grid(row=1,column=1)
 
 
 
