cb6402e0c71954c5d49a76e9300cdb9159d8cdc2
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-08-24 11:34:32 +0100

    add tkinter and window and frame setup

---
+++ main.py
@@ -0,0 +1,7 @@
+import tkinter 
+window = tkinter.Tk() #name root window 
+window.title("Data Entery form") #add title to window
+
+frame = tkinter.Frame(window) #create frame inside window
+
+window.mainloop() #come up with it
