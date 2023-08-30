627af2c7dad402c175222f492de8a69b13881dc3
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-08-30 12:32:25 +0100

    Modify submit padding and font

--- main_class.py
+++ main_class.py
@@ -57,7 +57,7 @@ class DataEntryApp:
         for widget in self.user_info_frame.winfo_children():
             widget.grid_configure(padx=10, pady=5)
 
-        self.submit_button = tk.Button(self.frame, text="Submit",fg="red", command=self.enter_data)
+        self.submit_button = tk.Button(self.frame, text="Submit",fg="#333333",padx=6,font=('arial',10), command=self.enter_data)
         self.submit_button.grid(row=3, column=0, pady=10)
 
     def create_courses_frame(self):
